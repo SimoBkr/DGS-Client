87935889b5f17d30a2fc7677c1a878c42f57d53d
SimoBkr <simobkr.sb@gmail.com>
2021-09-27 14:22:58 +0100

    first commit

---
+++ src/main/java/com/dgs/Dgs_Client/DgsClientApplication.java
@@ -0,0 +1,13 @@
+package com.dgs.Dgs_Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsClientApplication.class, args);
+	}
+
+}
